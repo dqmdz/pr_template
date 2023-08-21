bdc0916e9bcf40b46b8603038b053487535eca7c
dqmdz <daniel.quinterospinto@gmail.com>
2023-08-21 12:00:51 -0300

    Adding resta

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,15 @@
 import unittest
 
-from app.main import suma
+from app.main import suma, resta
 
 
 class TestApp(unittest.TestCase):
 
     def test_suma(self):
         self.assertEqual(suma(1, 3), 4)
+
+    def test_resta(self):
+        self.assertEqual(resta(3, 1), 2)
 
 
 if __name__ == '__main__':
